1532b55bbf8238557b813f1551e3774a1680d843
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-07-05 12:56:24 -0400

    parsing and error handling++

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,5 @@
 
 
-use std::path::PathBuf;
-
 use serde::{Serialize, Deserialize};
 
 
@@ -9,7 +7,41 @@ use serde::{Serialize, Deserialize};
 pub struct Config {
   //#[serde(default = "empty_string")]
   pub boot_iso_url: String,
-  pub boot_iso: PathBuf,
+  pub boot_iso: std::path::PathBuf,
 }
 
-
+pub fn read_all_config(dir: &std::path::Path) -> Vec<Config> {
+  let mut c = Vec::with_capacity(32);
+  match std::fs::read_dir(dir) {
+    Ok(read_dir) => {
+      for entry in read_dir {
+        match entry {
+          Ok(entry) => {
+            match std::fs::read_to_string(entry.path()) {
+              Ok(entry_content) => {
+                match toml::from_str::<Config>(&entry_content) {
+                  Ok(parsed_config) => {
+                    c.push(parsed_config);
+                  }
+                  Err(e) => {
+                    eprintln!("{:?} {:?}", entry.path(), e);
+                  }
+                }
+              }
+              Err(e) => {
+                eprintln!("{:?}", e);
+              }
+            }
+          }
+          Err(e) => {
+            eprintln!("{:?}", e);
+          }
+        }
+      }
+    }
+    Err(e) => {
+      eprintln!("{:?}", e);
+    }
+  }
+  c
+}
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ mod args;
 
 fn main() {
     let a = args::parse_cli_args();
-    a.maybe_log(1, || { eprintln!("a = {:?}", a);})
+    a.maybe_log(1, || { eprintln!("a = {:?}", a);});
+    let c = config::read_all_config(&a.config_folder);
+    a.maybe_log(1, || { eprintln!("c = {:?}", c);});
 
-    //let c = config::();
 }
 
 
